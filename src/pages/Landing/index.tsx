8b9acfe0db9247d7656c44d395b0f1e2e3738d57
Wesley S <wexxcode@gmail.com>
2020-08-04 13:33:34 -0300

    Adicionando rotas e páginas internas

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import logoImg from '../../assets/images/logo.svg'
 import landingImg from '../../assets/images/landing.svg';
+
 
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveCLassesIcon from '../../assets/images/icons/give-classes.svg';
@@ -22,14 +25,14 @@ function Landing() {
                     className="hero-image"
                 />
                 <div className="buttons-container">
-                    <a href="" className="study">
+                    <Link to="/study" className="study">
                         <img src={studyIcon} alt="Estudar" />
                         Estudar
-                    </a>
-                    <a href="" className="give-classes">
+                    </Link>
+                    <Link to="/give-classes" className="give-classes">
                         <img src={giveCLassesIcon} alt="Estudar" />
                         Dar Aulas
-                    </a>
+                    </Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões ja realizadas.
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, RouteProps } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import TeacherList from './pages/TeacherList';
 import TeacherForm from './pages/TeacherForm';
@@ -7,9 +7,9 @@ import TeacherForm from './pages/TeacherForm';
 function Routes() {
     return(
        <BrowserRouter>
-            <Route path="/" exact component={Landing}/>
-            <Route path="/study" component={TeacherList}/>
-            <Route path="/give-classes" component={TeacherForm}/>
+            <Route path="/" exact component={ Landing } />
+            <Route path="/study" component={ TeacherList } />
+            <Route path="/give-classes" component={ TeacherForm } />
        </BrowserRouter> 
     )
 }
